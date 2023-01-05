7a0ea01484564043f04bdeb2a76911484a564a16
RafalW <rafal.wielicki@gmail.com>
2023-01-05 10:30:39 +0100

    Add clients get method

--- src/controllers/clients.js
+++ src/controllers/clients.js
@@ -1,7 +1,10 @@
 const { logger } = require('../misc/logger');
 const {Client} = require('../models');
-const { Sequelize, Op } = require('sequelize');
+const { Sequelize, Op, QueryTypes } = require('sequelize');
 const tools = require('../misc/tools');
+const {sequelize} = require('../models');
+const sortOrder = require('../enums/sortOrder');
+const settlementType = require('../enums/settlementType');
 
 module.exports = {
   async getId (name) {
@@ -39,5 +42,59 @@ module.exports = {
     })
     .then((values) => res.send(values.map(u => u.name)))
     .catch((error) => tools.sendError(res, error));
+  },
+  async get (req, res) {
+    try {
+      console.log('query', req.query);
+
+      const page = req.query.page || 1;
+
+      // sorting columns
+      const sortColumn = req.query['sort-column'] ? req.query['sort-column'] : 'creationDate';
+      const order = sortOrder.getSqlKeyword(req.query['sort-order']);
+
+      // run query
+      sequelize.query(`
+        select
+          c.name as client,
+          sum(t.price) as amount
+        from Clients c
+        left join Tasks t on c.id = t.clientId
+        where
+          c.name like :search
+          and
+            case when :settlementType is not null then
+              case when :settlementType = :settled then
+                t.settlementDate is not null
+              else
+                case when :settlementType = :unsettled then
+                  t.settlementDate is null
+                end
+              end
+            else true end
+        group by c.name
+        order by ${sortColumn} ${order}
+        limit 50
+        offset :offset
+      `, {
+        type: QueryTypes.SELECT,
+        replacements: {
+          search: req.query.search ? `%${req.query.search}%` : '%%',
+          settlementType: req.query['settlement-type'] && parseInt(req.query['settlement-type']) !== settlementType.all ? parseInt(req.query['settlement-type']) : null,
+          settled: settlementType.settled,
+          unsettled: settlementType.unsettled,
+          offset: 50 * (page - 1),
+        },
+      })
+      .then((clients) => {
+        res.send({
+          clients,
+          meta: {
+            page: 1,
+          },
+        });
+      });
+    }
+    catch (error) { tools.sendError(res, error); }
   },
 }
--- src/routes/clients.js
+++ src/routes/clients.js
@@ -3,5 +3,6 @@ const authorizationMiddleware = require('../middlewares/authorization');
 
 module.exports = (app) => {
   app.get('/clients/names/distinct', authorizationMiddleware.filter, clients.getNamesDistinct);
+  app.get('/clients', authorizationMiddleware.filter, clients.get);
 
 }
