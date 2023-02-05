41dd906ed52dd5005abc5e5bbe4a66e0288fc9dd
RafalW <rafal.wielicki@gmail.com>
2023-02-05 16:12:52 +0100

    Update tableRelations.js

--- src/database/tableRelations.js
+++ src/database/tableRelations.js
@@ -1,28 +1,31 @@
 const {Task} = require('../models');
 const {Client} = require('../models');
 const {Project} = require('../models');
+const {logger} = require("../misc/logger");
 
 //Jesli jest blad to wyremuj na chwile. Musi istniec tabela
 
+logger.info('Creating table relations...');
+
 //client
+Task.belongsTo(Client, {
+  as: 'client',
+  foreignKey: 'clientId',
+});
 Client.hasMany(Task, {
   as: 'tasks',
   foreignKey: 'clientId',
   onDelete: 'SET NULL',
 });
-Task.belongsTo(Client, {
-  as: 'client',
-  foreignKey: 'clientId',
-});
 
 //project
+Task.belongsTo(Project, {
+  as: 'project',
+  foreignKey: 'projectId',
+});
 Project.hasMany(Task, {
   as: 'tasks',
   foreignKey: 'projectId',
   onDelete: 'SET NULL',
-});
-Task.belongsTo(Project, {
-  as: 'project',
-  foreignKey: 'projectId',
 });
 
