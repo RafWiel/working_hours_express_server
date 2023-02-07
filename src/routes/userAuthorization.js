07c955416a2e3300805f0c7a60a4cd10081e705b
RafalW <rafal.wielicki@gmail.com>
2023-02-07 09:38:24 +0100

    Update userAuthorization.js

--- src/routes/userAuthorization.js
+++ src/routes/userAuthorization.js
@@ -1,7 +1,6 @@
 const controller = require('../controllers/userAuthorization');
-const authorization = require('../middlewares/authorization');
 const policy = require('../policies/userAuthorization');
 
 module.exports = (app) => {
-  app.post(`/${process.env.URL_PATH}/login`, authorization.filter, policy.login, controller.login);
+  app.post(`/${process.env.URL_PATH}/login`, policy.login, controller.login);
 }
